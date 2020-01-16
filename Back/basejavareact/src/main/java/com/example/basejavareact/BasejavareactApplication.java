756c5623f7a7b5961d603afd95a651d11d045371
lherrerae <luis.herrerae@axity.com>
2020-01-16 13:48:41 -0600

    Base back-end

---
+++ Back/basejavareact/src/main/java/com/example/basejavareact/BasejavareactApplication.java
@@ -0,0 +1,13 @@
+package com.example.basejavareact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasejavareactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasejavareactApplication.class, args);
+	}
+
+}
