a01271b9315236417b062505c1c0a73540d30e20
lherrerae <luis.herrerae@axity.com>
2020-01-21 16:22:15 -0600

    Estructura base de datos

--- Back/basejavareact/src/main/resources/data.sql
+++ Back/basejavareact/src/main/resources/data.sql
@@ -1,13 +1,26 @@
 DROP TABLE IF EXISTS persons;
 
 CREATE TABLE persons (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL
+  last_name VARCHAR(100) NOT NULL,
+  birthdate DATE,
+  mail VARCHAR(30) NOT NULL,
+  sex VARCHAR(1) NOT NULL,
+  id_hobbie INT
 );
+
+CREATE TABLE hobbies (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+ALTER TABLE persons
+    ADD FOREIGN KEY (id_hobbie) 
+    REFERENCES hobbies(id);
  
-INSERT INTO persons (first_name, last_name) VALUES
-  ('Wes', 'Bos'),
-  ('Luis', 'Herrera'),
-  ('Ivan', 'Equihua'),
-  ('Dan', 'Abramov');
+-- INSERT INTO persons (first_name, last_name) VALUES
+--   ('Wes', 'Bos'),
+--   ('Luis', 'Herrera'),
+--   ('Ivan', 'Equihua'),
+--   ('Dan', 'Abramov');
