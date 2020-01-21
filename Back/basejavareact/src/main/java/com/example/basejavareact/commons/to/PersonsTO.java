349c82428b633ebcbdf7c4805af771e7830b6274
lherrerae <luis.herrerae@axity.com>
2020-01-21 17:05:29 -0600

    Actualizacion TO

--- Back/basejavareact/src/main/java/com/example/basejavareact/commons/to/PersonsTO.java
+++ Back/basejavareact/src/main/java/com/example/basejavareact/commons/to/PersonsTO.java
@@ -1,5 +1,9 @@
 package com.example.basejavareact.commons.to;
 
+import java.sql.Date;
+import java.util.List;
+import com.example.basejavareact.model.Hobbies;;
+
 /**
  * PersonsTO
  */
@@ -10,8 +14,12 @@ public class PersonsTO extends BaseTO {
      */
     private static final long serialVersionUID = 8324321294578528875L;
 
-    String firstName;
-    String lastName;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String mail;
+    private String sex;
+    private List<Hobbies> hobbies;
 
     public static long getSerialversionuid() {
         return serialVersionUID;
@@ -31,6 +39,38 @@ public class PersonsTO extends BaseTO {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public List<Hobbies> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<Hobbies> hobbies) {
+        this.hobbies = hobbies;
     }
 
 }
