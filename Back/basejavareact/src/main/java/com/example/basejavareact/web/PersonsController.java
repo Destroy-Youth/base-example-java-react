391b673ea655ed467b1b74b6a46b5e9bc36ecfd7
Ivan Equihua <luis.herrerae@outlook.com>
2020-03-04 23:28:45 -0600

    WIP: Awesome auto mapper config.

--- Back/basejavareact/src/main/java/com/example/basejavareact/web/PersonsController.java
+++ Back/basejavareact/src/main/java/com/example/basejavareact/web/PersonsController.java
@@ -12,7 +12,9 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import java.util.List;
 
 import com.example.basejavareact.commons.to.PersonsTO;
+import com.example.basejavareact.model.Persons;
 import com.example.basejavareact.services.service.IPersonsService;
+import com.example.basejavareact.util.DTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -36,6 +38,12 @@ public class PersonsController {
     @PostMapping(consumes = ALL_VALUE, produces = APPLICATION_JSON_VALUE, value = "/v1/persons")
     public ResponseEntity person(@RequestBody PersonsTO person) {
         personsService.savePerson(person);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = ALL_VALUE, produces = APPLICATION_JSON_VALUE, value = "/v1/test/personsDTO")
+    public ResponseEntity person(@DTO(PersonsTO.class) Persons person) {
+        personsService.savePersonEntity(person);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
